dc9fb0d794fba3d9b94b716768ccb6da3a0ad002
Tudor Palanga <tudor.palanga@gmail.com>
2017-02-17 22:32:41 +0000

    Add timeout for eventually and implement backend timeout test

--- dataservice/src/test/scala/com/tpalanga/dataservice/route/WebRouteSpec.scala
+++ dataservice/src/test/scala/com/tpalanga/dataservice/route/WebRouteSpec.scala
@@ -8,6 +8,7 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.testkit.TestProbe
 import com.tpalanga.dataservice.model.{User, UserDataStore}
 import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{FlatSpec, Matchers}
 
 
@@ -22,21 +23,43 @@ object WebRouteSpec {
 }
 
 class WebRouteSpec extends FlatSpec with ScalatestRouteTest with SprayJsonSupport with Matchers with Eventually {
+  import UserDataStore.DataFormats._
   import WebRouteSpec._
+
+  implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(2, Seconds)), interval = scaled(Span(100, Millis)))
 
   // TODO (TP): use a tree like spec (FeatureSpec?)
   "WebRoute" should "respond to a GET request with an existing user" in new Test {
-    Get(s"data/users/$userId") ~> route ~> check {
+    Get(s"/data/users/$userId") ~> route ~> check {
       userService.expectMsg(UserDataStore.GetOne(userId))
       userService.reply(UserDataStore.OneUser(testUser))
-      eventually { status shouldEqual StatusCodes.OK }
+      eventually {
+        status shouldEqual StatusCodes.OK
+      }
       responseAs[User] should be(testUser)
     }
   }
 
-  it should "respond to a GET request with NotFound if the user does not exist" in new Test {
-    userService.expectMsg(UserDataStore.GetOne(userId))
-    userService.reply(UserDataStore.NotFound)
-    eventually { status shouldEqual StatusCodes.NotFound }
+  it should "respond to a GET request with status 404 if the user does not exist" in new Test {
+    Get(s"/data/users/$userId") ~> route ~> check {
+      userService.expectMsg(UserDataStore.GetOne(userId))
+      userService.reply(UserDataStore.NotFound(userId))
+      eventually {
+        status shouldEqual StatusCodes.NotFound
+      }
+    }
   }
+
+  it should "respond to a GET request with status 500 if retrieving the user fails (times out)" in new Test {
+    // this timeout should be longer than the ask timeout
+    implicit val patienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)), interval = scaled(Span(100, Millis)))
+    Get(s"/data/users/$userId") ~> route ~> check {
+      userService.expectMsg(UserDataStore.GetOne(userId))
+      eventually {
+        status shouldEqual StatusCodes.InternalServerError
+      }
+      responseAs[String] should startWith(s"Getting user with ID $userId from userService failed: Ask timed out")
+    }
+  }
+
 }
