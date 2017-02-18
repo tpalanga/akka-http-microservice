2dd119d7fbd594de422e11ea9dbb6969aa89235e
Tudor Palanga <tudor.palanga@gmail.com>
2017-02-18 15:29:14 +0000

    Add actor to catch and log unhandled messages and dead letters

--- dataservice/src/main/scala/com/tpalanga/dataservice/Bootstrap.scala
+++ dataservice/src/main/scala/com/tpalanga/dataservice/Bootstrap.scala
@@ -5,12 +5,15 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.tpalanga.dataservice.model.UserDataStore
 import com.tpalanga.dataservice.route.WebRoute
+import com.tpalanga.dataservice.util.UnhandledMessageWatcher
 
 object Bootstrap extends App {
 
   implicit val system = ActorSystem("dataservice-system")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
+
+  val unhandledMessageWatcher = system.actorOf(UnhandledMessageWatcher.props())
 
   val userService = system.actorOf(UserDataStore.props())
   Http().bindAndHandle(new WebRoute(userService).route, "localhost", 8081).map { httpServerBinding =>
